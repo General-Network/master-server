df25c97b99d1150c39937fa48ff7b1a712fd83a0
Dominik <admin@dominik48n.de>
2022-05-09 17:47:04 +0200

    Implement config files

--- src/main/kotlin/de/dominik48n/generalnetwork/master/MasterServer.kt
+++ src/main/kotlin/de/dominik48n/generalnetwork/master/MasterServer.kt
@@ -1,11 +1,13 @@
 package de.dominik48n.generalnetwork.master
 
 import com.google.gson.GsonBuilder
+import de.dominik48n.generalnetwork.master.config.Document
 import de.dominik48n.generalnetwork.master.network.DefaultNetworkProvider
 import de.dominik48n.generalnetwork.master.network.handler.NetworkHandler
 import de.dominik48n.generalnetwork.master.network.packet.handler.PacketDecoder
 import de.dominik48n.generalnetwork.master.network.packet.handler.PacketEncoder
 import org.apache.log4j.Logger
+import java.io.File
 
 class MasterServer {
 
@@ -17,12 +19,14 @@ class MasterServer {
     val gson = GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create()
 
     lateinit var defaultNetworkProvider: DefaultNetworkProvider
+    lateinit var masterConfig: Document
 
     fun start() {
         this.logger.info("Starting Master Server...")
 
         INSTANCE = this
 
+        this.prepareConfig()
         this.prepareNettyServer()
     }
 
@@ -32,11 +36,19 @@ class MasterServer {
         this.defaultNetworkProvider.nettyServer.stopServer()
     }
 
+    private fun prepareConfig() {
+        val configFile = File("master", "config.json")
+
+        this.masterConfig = Document.read(configFile) ?: Document.create(configFile)
+    }
+
     private fun prepareNettyServer() {
         this.defaultNetworkProvider = DefaultNetworkProvider()
         this.defaultNetworkProvider.start()
 
-        this.defaultNetworkProvider.nettyServer.startServer(9967) {
+        this.defaultNetworkProvider.nettyServer.startServer(
+            this.masterConfig.getDocument("network").getIntValue("master-port")
+        ) {
             it.pipeline()
                 .addLast(PacketEncoder(this.defaultNetworkProvider.packetRegistry))
                 .addLast(PacketDecoder(this.defaultNetworkProvider.packetRegistry))
