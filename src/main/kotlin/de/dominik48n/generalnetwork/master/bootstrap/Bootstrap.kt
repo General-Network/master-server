36ec9a3957fe2e9bfa7c3446558ef6936e95488d
Dominik <admin@dominik48n.de>
2022-05-11 07:51:18 +0200

    Fix shutdown hook

--- src/main/kotlin/de/dominik48n/generalnetwork/master/bootstrap/Bootstrap.kt
+++ src/main/kotlin/de/dominik48n/generalnetwork/master/bootstrap/Bootstrap.kt
@@ -5,11 +5,7 @@ import de.dominik48n.generalnetwork.master.MasterServer
 fun main() {
     val masterServer = MasterServer()
 
-    Thread({
-        masterServer.start()
+    Thread({ masterServer.start() }, "master-server-main").start()
 
-        Runtime.getRuntime().addShutdownHook(Thread({
-            masterServer.stop()
-        }, "master-server-stop"))
-    }, "master-server-main").start()
+    Runtime.getRuntime().addShutdownHook(Thread({ masterServer.stop() }, "master-server-stop"))
 }
